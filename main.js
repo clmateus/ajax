547264ee43a10b2b8cc1362278ab2da8fe53c2c0
clmateus <cl.mateuscosta@gmail.com>
2025-06-23 16:57:20 -0300

    updates

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
-    document.querySelector("#btn buscar-cep").addEventListener('click', function(){
-        
+    document.querySelector("#btn-buscar-cep").addEventListener('click', function(){
+        const xhttp = new XMLHttpRequest();
+        const cep = document.querySelector("#cep").value;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        xhttp.open('GET', endpoint);
+        xhttp.send();
     })
 })
