ba9d4a7148e0bd7b5a96a095cf5d5199bedeb46f
clmateus <cl.mateuscosta@gmail.com>
2025-06-24 22:53:43 -0300

    updated to fetch

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("#btn-buscar-cep").addEventListener('click', function(){
-        const xhttp = new XMLHttpRequest();
-        const cep = document.querySelector("#cep").value;
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        // const xhttp = new XMLHttpRequest();
+        // const cep = document.querySelector("#cep").value;
+        // const endpoint = `https://viacep.com.br/ws/${cep}/json`;
 
-        xhttp.open('GET', endpoint);
-        xhttp.send();
+        // xhttp.open('GET', endpoint);
+        // xhttp.send();
+        
+        const cep = document.querySelector("#cep").value;
+
+        fetch(`https://viacep.com.br/ws/${cep}/json`)
+            .then(response => response.json())
+            .then(dados => {
+                document.querySelector("#endereco").value = dados.logradouro;
+                console.log(dados)
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+            })
     })
 })
